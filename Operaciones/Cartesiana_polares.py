fa6f0f1ca4fde07ee3c1cd1614ec5bb7eaf77171
Jacob202122 <157903207+Jacob202122@users.noreply.github.com>
2024-08-20 21:18:54 -0500

    Update Cartesiana_polares.py

--- Operaciones/Cartesiana_polares.py
+++ Operaciones/Cartesiana_polares.py
@@ -1,21 +1,15 @@
 import math
 
-def Modulo(a):
+def Cartesianas_a_polares(a):
     p = (a[0]**2 + a[1]**2)**(1/2)
-    
-    return p
-
-def Angulo(a):
     teta = math.atan2(a[1],a[0])
-    return teta
     
+    return p, teta
     
 def prettyPrinting(r):
-    
-    p = Modulo(r)
-    teta = Angulo(r)
-    print('(',p,',',teta,'rad )')
+
+    print('(',r[0],',',r[1],'rad )')
 
 if __name__ == "__main__":
     
-    prettyPrinting([1,1])  
+    prettyPrinting(Cartesianas_a_polares([1,1]))  
