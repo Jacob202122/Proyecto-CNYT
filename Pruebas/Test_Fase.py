9a07d63d8174ff712cdf724fcb2779faa489cfcc
Jacob202122 <157903207+Jacob202122@users.noreply.github.com>
2024-08-20 21:11:00 -0500

    Create Test_Fase.py

---
+++ Pruebas/Test_Fase.py
@@ -0,0 +1,17 @@
+def fase(a):
+    
+    teta = math.atan2(a[1],a[0])
+    
+    return teta
+    
+class TestFase(unittest.TestCase):
+    
+    def test_fase_1(self):
+        self.assertEqual(fase([1,1]), math.pi/4)
+    
+    def test_fase_1(self):
+        self.assertEqual(fase([-1,0]), math.pi)
+
+if __name__ == "__main__":
+    
+    unittest.main()
