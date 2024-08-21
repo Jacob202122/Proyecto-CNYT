999a4b5c51b1d1f06033c95512e7dfc0affdb209
Jacob202122 <157903207+Jacob202122@users.noreply.github.com>
2024-08-20 20:23:31 -0500

    Update Test_Conjugado.py

--- Pruebas/Test_Conjugado.py
+++ Pruebas/Test_Conjugado.py
@@ -1,1 +1,20 @@
+import unittest
 
+def Conjugado(a):
+    
+    RealP = a[0]
+    ImaginaryP = -a[1]
+
+    return RealP, ImaginaryP
+
+class TestConjugado(unittest.TestCase):
+    
+    def Test_Conjugado_1(self):
+        self.assertEqual(Conjugado([1,2]),(1,-2))
+    
+    def Test_Conjugado_2(self):
+        self.assertEqual(Conjugado([7,-2]),(7,2))
+
+
+if __name__ == '__main__':
+    unittest.main()
