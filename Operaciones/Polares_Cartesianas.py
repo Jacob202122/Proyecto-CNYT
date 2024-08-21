1412f832aa1672867c5485461889c2fafec5f484
Jacob202122 <157903207+Jacob202122@users.noreply.github.com>
2024-08-20 20:59:01 -0500

    Create Polares_Cartesianas.py

---
+++ Operaciones/Polares_Cartesianas.py
@@ -0,0 +1,19 @@
+import math
+
+# De cartesiana a polares
+def PolaresACartesianas(polar):
+    
+    a = polar[0]*math.cos(polar[1])
+    
+    b = polar[0]*math.sin(polar[1])
+    
+    return a, b 
+    
+def prettyPrinting(r):
+    
+    print(r[0],'+',r[1],'i')
+
+if __name__ == "__main__":
+    
+    resultado = PolaresACartesianas([2**(1/2),math.pi/4])
+    prettyPrinting(resultado)
