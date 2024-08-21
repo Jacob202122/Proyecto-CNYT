f8df04829e8d124319f29f5aa37c20149eec437b
Jacob202122 <157903207+Jacob202122@users.noreply.github.com>
2024-08-20 21:28:13 -0500

    Update Test_Polares_Cartesianas.py

--- Pruebas/Test_Polares_Cartesianas.py
+++ Pruebas/Test_Polares_Cartesianas.py
@@ -1,1 +1,21 @@
+import math
+import unittest
 
+def PolaresACartesianas(polar):
+    
+    a = polar[0]*math.cos(polar[1])
+    b = polar[0]*math.sin(polar[1])
+    
+    return a, b 
+    
+class Test_Polares_Cartesianas(unittest.TestCase):
+    
+    def Test_transformacion_1(self):
+        self.assertEqual(PolaresACartesianas([2**1/2,math.pi/4]),(1,1))
+    
+    def Test_transformacion_1(self):
+        self.assertEqual(PolaresACartesianas([1,math.pi]),(0,1))
+
+
+if __name__ == "__main__":
+    unittest.main()
