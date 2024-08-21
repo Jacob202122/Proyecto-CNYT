84f14a662d8d679ba8fea5bc4b45faeeba576ddc
Jacob202122 <157903207+Jacob202122@users.noreply.github.com>
2024-08-20 20:30:19 -0500

    Update Test_Modulo.py

--- Pruebas/Test_Modulo.py
+++ Pruebas/Test_Modulo.py
@@ -1,1 +1,18 @@
+import unittest
+
+def Modulo(a):
+    r = (a[0]**2 + a[1]**2)**(1/2)
+    
+    return r
+
+class TestModulo(unittest.TestCase):
+    
+    def test_modulo_1(self):
+        self.assertEqual(Modulo([4,3]),5)
+    
+    def test_modulo_1(self):
+        self.assertEqual(Modulo([6,8]),10)
+        
+if __name__ == '__main__':
+    unittest.main()
 
